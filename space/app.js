d468087e9562aca757a99bef252d4b536d1e59c9
pengbinbin <huiling3240@icloud.com>
2017-03-10 16:55:15 +0800

    add search form, but not test

--- space/app.js
+++ space/app.js
@@ -1,5 +1,16 @@
 //app.js
 App({
+  data: {
+    iconSize: [20, 30, 40, 50, 60, 70],
+    iconColor: [
+      'red', 'orange', 'yellow', 'green', 'rgb(0,255,255)', 'blue', 'purple'
+    ],
+    iconType: [
+      'success', 'info', 'warn', 'waiting', 'safe_success', 'safe_warn',
+      'success_circle', 'success_no_circle', 'waiting_circle', 'circle', 'download',
+      'info_circle', 'cancel', 'search', 'clear'
+    ]
+  },
   onLaunch: function () {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
